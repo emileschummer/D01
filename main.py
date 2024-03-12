114cc2d04ae5a110009031332b2aa0bf409e5f54
Lcalderon <L.CalderondelRio@student.tudelft.nl>
2024-03-12 11:45:43 +0100

    Added main.py template

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+"""
+
+This is the main run file
+
+"""
+
+# Import the necessary libraries
+
+
+# Select the plane of interest and propeller configuration.
+
+
+# Extract the data
+
+    # Obtain unsteady flow field
+
+    # Convert into time averaged flow fields
+
+    # Convert into bin averaged flow fields
+
+
+# Make fields into 3D arrays
+
+
+# Obtain the flow properties for both bin averaged and time averaged flow fields
+
+
+# Plot the flow properties / Visualize
