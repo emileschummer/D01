cbe75cb91b96503a23ff3fd3f7580835c05a33e5
Wisse <wissevries2000@Gmail.com>
2024-03-25 14:25:16 +0100

    sync

--- Vorticity_image_gen.py
+++ Vorticity_image_gen.py
@@ -5,21 +5,21 @@ import os
 from matplotlib.cm import ScalarMappable
 
 
-def Vorticity_image(u_magnitudes, v_magnitudes, plane, J_number):
-    print(u_magnitudes, v_magnitudes)
+def Vorticity_image(u_magnitudes, v_magnitudes, plane, J_number, bin):
+    
     #acquiring positions
     positions_file_path = "B_J1/XY.dat"
     positions = np.loadtxt(positions_file_path)  
     # Read data from files
     # Extract x, y positions from the positions data
     x_positions = positions[:, 0]
-    print(x_positions)
+    
     y_positions = positions[:, 1]
-    print(y_positions)
+    
     
     # Calculate the partial derivatives of the velocity field, axis 1 is x, axis 0 is y
     dVx_dy = np.gradient(u_magnitudes)
-    print(dVx_dy)
+    
     dVy_dx = np.gradient(v_magnitudes)
     
     #print(dVy_dx[1500])
@@ -34,17 +34,17 @@ def Vorticity_image(u_magnitudes, v_magnitudes, plane, J_number):
 
     
     
-     # Define colormap from dark blue to bright red
+    # Define colormap from dark blue to bright red
     cmap = plt.colormaps.get_cmap('gist_rainbow')
 
     # Normalize magnitudes to range from 0 to 1
-    norm = Normalize(vmin=-10, vmax=30)
+    norm = Normalize(vmin=-2, vmax=2)
 
     # Set figure size and DPI for high-quality image
     fig, ax = plt.subplots(figsize=(12, 8), dpi=300)
 
     # Plotting Vector Field with QUIVER and colormap
-    ax.scatter(x_positions, y_positions, s=Vorticity_field, cmap='viridis')
+    ax.scatter(x_positions, y_positions, s=Vorticity_field, c=Vorticity_field, cmap=cmap, norm=norm)
     ax.set_title('Vector Field with Color Scale')
 
     # Create a ScalarMappable object for colormap
@@ -69,7 +69,7 @@ def Vorticity_image(u_magnitudes, v_magnitudes, plane, J_number):
     # Create directory for storing images if it doesn't exist
     output_directory = f"Results\{plane}\J{J_number}\Vorticity_fields"
     os.makedirs(output_directory, exist_ok=True)
-    
+    bin=10
     # Save the figure as a high-quality image in the output directory
     output_path = os.path.join(output_directory, f'bin_{bin}.png')
     plt.savefig(output_path, bbox_inches='tight')
@@ -78,3 +78,102 @@ def Vorticity_image(u_magnitudes, v_magnitudes, plane, J_number):
     # Close the plot to release memory
     plt.close()
 
+
+
+def Velocity_fluctuations_image(u_magnitudes, v_magnitudes, average_U_arr, average_V_arr, plane, J_number, bin):
+    # Check that both arrays are the same shape
+
+    # Calculate the difference between the instantaneous velocity field and the mean velocity field
+    Velocity_fluctuations_u = u_magnitudes - average_U_arr
+    Velocity_fluctuations_v = v_magnitudes - average_V_arr
+
+    #acquiring positions
+    positions_file_path = "B_J1/XY.dat"
+    positions = np.loadtxt(positions_file_path)  
+    # Read data from files
+    # Extract x, y positions from the positions data
+    x_positions = positions[:, 0]
+    y_positions = positions[:, 1]
+    
+    # Define colormap from dark blue to bright red
+    cmap = plt.colormaps.get_cmap('gist_rainbow')
+
+    # Normalize magnitudes to range from 0 to 1
+    norm = Normalize(vmin=-2, vmax=2)
+
+    
+    
+    # Set figure size and DPI for high-quality image
+    fig, ax = plt.subplots(figsize=(12, 8), dpi=300)
+
+    # Plotting Vector Field with QUIVER and colormap
+    ax.scatter(x_positions, y_positions, c=Velocity_fluctuations_u, cmap=cmap, norm=norm)
+    ax.set_title('Vector Field with Color Scale')
+
+    # Create a ScalarMappable object for colormap
+    sm = ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])  # Pass an empty array
+
+    # Add colorbar using the ScalarMappable object
+    cbar = plt.colorbar(sm, ax=ax)
+    cbar.set_label('Magnitude of velocity fluctations U')
+    
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_title('Scatter plot U fluctations')
+
+    # Setting x, y boundary limits
+    ax.set_xlim(np.min(x_positions) - 1, np.max(x_positions) + 1)
+    ax.set_ylim(np.min(y_positions) - 1, np.max(y_positions) + 1)
+
+    # Show plot with grid
+    ax.grid()
+
+    # Create directory for storing images if it doesn't exist
+    output_directory = f"Results\{plane}\J{J_number}\Fluctations_fields"
+    os.makedirs(output_directory, exist_ok=True)
+    
+    # Save the figure as a high-quality image in the output directory
+    output_path = os.path.join(output_directory, f'bin_{bin}_U.png')
+    plt.savefig(output_path, bbox_inches='tight')
+
+
+    # Close the plot to release memory
+    plt.close()
+        # Set figure size and DPI for high-quality image
+    fig, ax = plt.subplots(figsize=(12, 8), dpi=300)
+
+    # Plotting Vector Field with QUIVER and colormap
+    ax.scatter(x_positions, y_positions, c=Velocity_fluctuations_v, cmap=cmap, norm=norm)
+    ax.set_title('Vector Field with Color Scale')
+
+    # Create a ScalarMappable object for colormap
+    sm = ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])  # Pass an empty array
+
+    # Add colorbar using the ScalarMappable object
+    cbar = plt.colorbar(sm, ax=ax)
+    cbar.set_label('Magnitude of velocity fluctations V')
+    
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_title('Scatter plot V fluctations')
+
+    # Setting x, y boundary limits
+    ax.set_xlim(np.min(x_positions) - 1, np.max(x_positions) + 1)
+    ax.set_ylim(np.min(y_positions) - 1, np.max(y_positions) + 1)
+
+    # Show plot with grid
+    ax.grid()
+
+    # Create directory for storing images if it doesn't exist
+    output_directory = f"Results\{plane}\J{J_number}\Fluctations_fields"
+    os.makedirs(output_directory, exist_ok=True)
+    
+    # Save the figure as a high-quality image in the output directory
+    output_path = os.path.join(output_directory, f'bin_{bin}_V.png')
+    plt.savefig(output_path, bbox_inches='tight')
+
+
+    # Close the plot to release memory
+    plt.close()
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from averagefunction import average_values
 from Bin_average_function import bin_average_velocities
 from vorticity_fluctuations_KE_functions import Vorticity, Velocity_fluctuations, Turbulent_kinetic_energy
-from Vorticity_image_gen import Vorticity_image
+from Vorticity_image_gen import Vorticity_image, Velocity_fluctuations_image
 
 # Select the plane of interest and propeller configuration.
 
@@ -29,7 +29,9 @@ u_magnitudes, v_magnitudes = bin_average_velocities(10, 'B', 1)
 
 
 # Obtain the flow properties for bin averaged flow fields and plot the flow properties / Visualize
-Vorticity_image(u_magnitudes, v_magnitudes, 'B', 1)
+#Vorticity_image(u_magnitudes, v_magnitudes, 'B', 1)
+
+Velocity_fluctuations_image(u_magnitudes, v_magnitudes, average_U_arr, average_V_arr, 'B', 1, 10)
 """Velocity_fluctuations_u, Velocity_fluctuations_v = Velocity_fluctuations(u_magnitudes, v_magnitudes, average_U_arr, average_V_arr)
 print(Turbulent_kinetic_energy(Velocity_fluctuations_u, Velocity_fluctuations_v))"""
 
