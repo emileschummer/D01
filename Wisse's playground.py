0c86bad017df0bf729e8dd0104b9e7e98bf0db15
Wisse <wissevries2000@Gmail.com>
2024-03-25 17:31:17 +0100

    sync

--- Wisse's playground.py
+++ Wisse's playground.py
@@ -1,5 +1,7 @@
 from Bin_average_function import bin_average_vector_field
 from Bin_average_function import bin_average_vector_field_image
+from Bin_average_function import bin_average_velocities
+from Vorticity_image_gen import Vorticity_image
 
-
-print('pizza')
+u_magnitudes, v_magnitudes = bin_average_velocities(13, 'B', 1)
+Vorticity_image(u_magnitudes, v_magnitudes, 'B', 1, 13)
