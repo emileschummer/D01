165d614ead1f897e2df1566b11fc0e95dedef733
Wisse <wissevries2000@Gmail.com>
2024-03-25 13:51:58 +0100

    sync

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 from averagefunction import average_values
 from Bin_average_function import bin_average_velocities
 from vorticity_fluctuations_KE_functions import Vorticity, Velocity_fluctuations, Turbulent_kinetic_energy
+from Vorticity_image_gen import Vorticity_image
 
 # Select the plane of interest and propeller configuration.
 
@@ -21,17 +22,16 @@ from vorticity_fluctuations_KE_functions import Vorticity, Velocity_fluctuations
     # Obtain unsteady flow field
 
     # Convert into time averaged flow fields
-average_U_arr, average_V_arr = average_values(1, 4000, 'B', 1)
+average_U_arr, average_V_arr = average_values(1, 40, 'B', 1)
 
     # Convert into bin averaged flow fields
 u_magnitudes, v_magnitudes = bin_average_velocities(10, 'B', 1)
 
 
 # Obtain the flow properties for bin averaged flow fields and plot the flow properties / Visualize
-Vorticity(u_magnitudes, v_magnitudes)
-
-#Velocity_fluctuations_u, Velocity_fluctuations_v = Velocity_fluctuations(u_magnitudes, v_magnitudes, average_U_arr, average_V_arr)
-#print(Turbulent_kinetic_energy(Velocity_fluctuations_u, Velocity_fluctuations_v))
+Vorticity_image(u_magnitudes, v_magnitudes)
+"""Velocity_fluctuations_u, Velocity_fluctuations_v = Velocity_fluctuations(u_magnitudes, v_magnitudes, average_U_arr, average_V_arr)
+print(Turbulent_kinetic_energy(Velocity_fluctuations_u, Velocity_fluctuations_v))"""
 
 
 
