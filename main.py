ae04b022cf351d43bdf334bc17d07bbf60e1c60d
Wisse <wissevries2000@Gmail.com>
2024-03-26 09:43:57 +0100

    sync

--- main.py
+++ main.py
@@ -23,25 +23,29 @@ from Vorticity_image_gen import Vorticity_image, Velocity_fluctuations_image, Tu
 
     # Convert into time averaged flow fields
 
-    
-average_U_arr, average_V_arr = average_values(1, 4000, 'B', 1)
-time_average_image(average_U_arr, average_V_arr, 'B', 1)
+Planes=['A', 'B', 'C']
 
-    # Convert into bin averaged flow fields
-for i in range(1, 37):
-    u_magnitudes, v_magnitudes = bin_average_velocities(i, 'B', 1)
-    if u_magnitudes==0:
-        break
+for plane in Planes:
+    print(plane)
+    for j in range(0,2):    
+        average_U_arr, average_V_arr = average_values(1, 4000, plane, j)
+        time_average_image(average_U_arr, average_V_arr, plane, j)
+
+            # Convert into bin averaged flow fields
+        for i in range(1, 37):
+            u_magnitudes, v_magnitudes = bin_average_velocities(i, plane, j)
+            if u_magnitudes==0:
+                break
 
 
-# Obtain the flow properties for bin averaged flow fields and plot the flow properties / Visualize
-    Vorticity_image(u_magnitudes, v_magnitudes, 'B', 1, i)
-    # fluctuations_image
-    Velocity_fluctuations_image(u_magnitudes, v_magnitudes, average_U_arr, average_V_arr, 'B', 1, i)
-    # Turbulent kinetic energy
-    Turbulent_kinetic_energy(u_magnitudes, v_magnitudes, average_U_arr, average_V_arr, 'B', 1, i)
-    #vector fields
-    bin_average_vector_field_image(i, 'B', 1)
+        # Obtain the flow properties for bin averaged flow fields and plot the flow properties / Visualize
+            Vorticity_image(u_magnitudes, v_magnitudes, plane, j, i)
+            # fluctuations_image
+            Velocity_fluctuations_image(u_magnitudes, v_magnitudes, average_U_arr, average_V_arr, plane, j, i)
+            # Turbulent kinetic energy
+            Turbulent_kinetic_energy(u_magnitudes, v_magnitudes, average_U_arr, average_V_arr, plane, j, i)
+            #vector fields
+            bin_average_vector_field_image(i, plane, j)
     
 
 
